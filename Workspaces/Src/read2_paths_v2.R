b76492d78482bd931973076323e6d8de61ab3825
Mario Antonioletti <mario@epcc.ed.ac.uk>
2021-07-29 09:52:18 +0100

    Make reading in of data quieter by using show_col_types = FALSE.

--- Workspaces/Src/read2_paths_v2.R
+++ Workspaces/Src/read2_paths_v2.R
@@ -50,7 +50,7 @@ function(ordered_alldata = "", gs_locs = "", sets_from = "workspace",
         }
         if (sets_from == "directory") {
             moe <- strsplit(ps[i], split = "[/.]")[[1]][1]
-            genes <- as.numeric(vroom(ps[i]))
+            genes <- as.numeric(vroom(ps[i], show_col_types = FALSE))
         }
         paths_names <- c(paths_names, moe)
         per_orids_list <- c(per_orids_list, paste("per_orids_",
